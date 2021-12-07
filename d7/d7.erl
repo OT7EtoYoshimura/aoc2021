f68c1d4c747c655211bcba648927e173e0cdc077
OT7EtoYoshimura <70547450+OT7EtoYoshimura@users.noreply.github.com>
2021-12-07 17:06:14 +0100

    Use min(ceil(mean), floor(mean)) instead of bruteforcing

--- d7/d7.erl
+++ d7/d7.erl
@@ -3,25 +3,28 @@
 -define(FILENAME, 'd7.in').
 
 main() ->
-	{_, Raw}    = file:read_file(?FILENAME),
-	SortedCrabs = lists:sort(lists:map(fun binary_to_integer/1, string:lexemes(Raw, ",\n"))),
-	Median      = lists:nth(round(length(SortedCrabs)/2), SortedCrabs),
-	{Fuel1, _}  =  lists:foldl(fun total_fuel1/2, {0, Median}, SortedCrabs),
-	PropList    = [lists:foldl(fun total_fuel2/2, {0, Pos   }, SortedCrabs) || Pos <- lists:seq(hd(SortedCrabs), lists:last(SortedCrabs))],
-	Fuel2       = lists:min(proplists:get_keys(PropList)),
+	{_, Raw}   = file:read_file(?FILENAME),
+	CrabsPos   = lists:map(fun binary_to_integer/1, string:lexemes(Raw, ",\n")),
+	CrabsCount = length(CrabsPos),
+	Median     = lists:nth(round(CrabsCount/2), lists:sort(CrabsPos)),
+	MeanFloor  = floor(lists:sum(CrabsPos) / CrabsCount),
+	MeanCeil   =  ceil(lists:sum(CrabsPos) / CrabsCount),
+	Fuel1      =     element(1, lists:foldl(fun total_fuel1/2, {0, Median   }, CrabsPos)),
+	Fuel2      = min(element(1, lists:foldl(fun total_fuel2/2, {0, MeanCeil }, CrabsPos)),
+	                 element(1, lists:foldl(fun total_fuel2/2, {0, MeanFloor}, CrabsPos))),
 	{Fuel1, Fuel2}.
 
 total_fuel1(Pos, {Fuel, Median}) ->
 	{Fuel + abs(Median-Pos), Median}.
 
-total_fuel2(Pos, {Fuel, Goal}) ->
-	{Fuel + crab_fuel2(Pos, Goal), Goal}.
+total_fuel2(Pos, {Fuel, Mean}) ->
+	{Fuel + crab_fuel2(Pos, Mean), Mean}.
 
-crab_fuel2(Pos, Goal) ->
-	crab_fuel2(Pos, Goal, 1, 0).
-crab_fuel2(Pos, Goal, _Inc, Fuel) when Pos =:= Goal ->
+crab_fuel2(Pos, Mean) ->
+	crab_fuel2(Pos, Mean, 1, 0).
+crab_fuel2(Pos, Mean, _Inc, Fuel) when Pos =:= Mean ->
 	Fuel;
-crab_fuel2(Pos, Goal, Incr, Fuel) when Pos > Goal ->
-	crab_fuel2(Pos-1, Goal, Incr+1, Fuel+Incr);
-crab_fuel2(Pos, Goal, Incr, Fuel) when Pos < Goal ->
-	crab_fuel2(Pos+1, Goal, Incr+1, Fuel+Incr).
+crab_fuel2(Pos, Mean, Incr, Fuel) when Pos > Mean ->
+	crab_fuel2(Pos-1, Mean, Incr+1, Fuel+Incr);
+crab_fuel2(Pos, Mean, Incr, Fuel) when Pos < Mean ->
+	crab_fuel2(Pos+1, Mean, Incr+1, Fuel+Incr).
