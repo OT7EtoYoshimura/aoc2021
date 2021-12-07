beb02abeb1c11ca485bc594b1fc18f6d1d9cec9d
OT7EtoYoshimura <70547450+OT7EtoYoshimura@users.noreply.github.com>
2021-12-07 17:47:48 +0100

    Simplify per crab fuel calculation for p2

--- d7/d7.erl
+++ d7/d7.erl
@@ -1,6 +1,6 @@
 -module(d7).
 -export([main/0]).
--define(FILENAME, 'd7.in').
+-define(FILENAME, 'd7_bigboi.in').
 
 main() ->
 	{_, Raw}   = file:read_file(?FILENAME),
@@ -9,22 +9,14 @@ main() ->
 	Median     = lists:nth(round(CrabsCount/2), lists:sort(CrabsPos)),
 	MeanFloor  = floor(lists:sum(CrabsPos) / CrabsCount),
 	MeanCeil   =  ceil(lists:sum(CrabsPos) / CrabsCount),
-	Fuel1      =     element(1, lists:foldl(fun total_fuel1/2, {0, Median   }, CrabsPos)),
-	Fuel2      = min(element(1, lists:foldl(fun total_fuel2/2, {0, MeanCeil }, CrabsPos)),
-	                 element(1, lists:foldl(fun total_fuel2/2, {0, MeanFloor}, CrabsPos))),
+	Fuel1      =     element(1, lists:foldl(fun fuel1/2, {0, Median   }, CrabsPos)),
+	Fuel2      = min(element(1, lists:foldl(fun fuel2/2, {0, MeanCeil }, CrabsPos)),
+	                 element(1, lists:foldl(fun fuel2/2, {0, MeanFloor}, CrabsPos))),
 	{Fuel1, Fuel2}.
 
-total_fuel1(Pos, {Fuel, Median}) ->
+fuel1(Pos, {Fuel, Median}) ->
 	{Fuel + abs(Median-Pos), Median}.
 
-total_fuel2(Pos, {Fuel, Mean}) ->
-	{Fuel + crab_fuel2(Pos, Mean), Mean}.
-
-crab_fuel2(Pos, Mean) ->
-	crab_fuel2(Pos, Mean, 1, 0).
-crab_fuel2(Pos, Mean, _Inc, Fuel) when Pos =:= Mean ->
-	Fuel;
-crab_fuel2(Pos, Mean, Incr, Fuel) when Pos > Mean ->
-	crab_fuel2(Pos-1, Mean, Incr+1, Fuel+Incr);
-crab_fuel2(Pos, Mean, Incr, Fuel) when Pos < Mean ->
-	crab_fuel2(Pos+1, Mean, Incr+1, Fuel+Incr).
+fuel2(Pos, {Fuel, Mean}) ->
+	Goal = abs(Mean-Pos),
+	{Fuel + round(Goal*(Goal+1)/2), Mean}.
