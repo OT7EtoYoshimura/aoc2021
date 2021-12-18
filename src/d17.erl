fe36acac643e351a341e3640159f1b6215bf02e6
OT7EtoYoshimura <70547450+OT7EtoYoshimura@users.noreply.github.com>
2021-12-18 13:37:55 +0100

    d17: simplify defines

--- src/d17.erl
+++ src/d17.erl
@@ -1,20 +1,16 @@
 -module(d17).
--export([main/0, main/4]).
+-export([main/0, main/1]).
 -include("../include/aoc2021.hrl").
--define(XMIN, 117).
--define(XMAX, 164).
--define(YMIN,-140).
--define(YMAX, -89).
+-define(RANGE, {{117,164},{-140,-89}}).
 
 main() ->
-    main(?XMIN,?XMAX,?YMIN,?YMAX).
-main(XMin,XMax,YMin,YMax) ->   
+    main(?RANGE).
+main(Range = {{_XMin,XMax},{YMin,_YMax}}) ->   
     Vels  = [{XVel,YVel} ||
              XVel <- lists:seq(XMax, 0, -1),
              YVel <- lists:seq(abs(YMin), YMin, -1)],
-    Range = {{XMin, XMax}, {YMin, YMax}},
     Hits  = lists:filter(fun(V) -> m_throw(V, Range) end, Vels),
-    MaxYV = abs(?YMIN)-1,
+    MaxYV = abs(YMin)-1,
     {{p1, ints_sum(MaxYV)}, {p2, length(Hits)}}.
 
 m_throw({XVel,YVel}, Range)  ->
@@ -30,6 +26,6 @@ m_throw(Coords, Vels, Range) ->
     m_throw(NCoords, NVels, Range).
 
 tick({X,Y}, {0   ,YVel}) ->
-    {{X+XVel, Y+YVel}, {XVel  , YVel-1}};
+    {{X     , Y+YVel}, {0    , YVel-1}};
 tick({X,Y}, {XVel,YVel}) ->
     {{X+XVel, Y+YVel}, {XVel-1, YVel-1}}.
